825d9eb455cbc543dc67bab618e8f81e8ce049fa
pyjonhact <76703308+pyjonhact@users.noreply.github.com>
2020-12-28 17:50:25 -0300

    Update __main__.py

--- pyspikon/__main__.py
+++ pyspikon/__main__.py
@@ -14,9 +14,13 @@ EN: Convert .llsp files into .py codes''')
 parser.add_argument('path', metavar='P', type=str,
                      help='''PT-BR: O caminho para o arquivo.
 EN: The path to the file.''')
+parser.add_argument('pypath', metavar='C', type=str,
+                     help='''PT-BR: O repositorio onde o arquivo .py vai ser criado.
+EN: The repository where the .py file will be created.''')
 
 #Lê os argumentos extrai o arquivo project.json e o converte para .py
 args = parser.parse_args()
 p=convert.convert(openzip.extractandopen(args.path))
-pythonfile = open('output.py', 'w+', encoding='utf-8')
+c=args.pypath
+pythonfile = open(c+'\output.py', 'w+', encoding='utf-8')
 pythonfile.write(p)
