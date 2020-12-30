ed1789f68c959d59d191fd4b5a05e16329641825
PyJonhAct <76703308+pyjonhact@users.noreply.github.com>
2020-12-30 15:52:54 -0300

    Update and rename pyspikon/convert.py to Pyspikon/convert.py

--- Pyspikon/convert.py
+++ Pyspikon/convert.py
@@ -5,9 +5,10 @@
 
 #Importa blibliotecas
 import json
+import numpy
 
 #Define a lista de conversão de funções
-functionslist=[['flippermotor_motorGoDirectionToPosition', 'motor.run_to_position()'], ['flippermotor_motorStartDirection', 'motor.start()'], ['flippermotor_motorSetSpeed', 'motor.set_default_speed()'], ['flippermove_move', 'motor_pair.move()'], ['flippermove_steer', 'motor_pair.move_tank()'], ['flippermove_startSteer', 'motor_pair.start_tank()'], ['flippermove_stopMove', 'motor_pair.stop()'], ['flippermove_movementSpeed', 'motor_pair.set_default_speed()'], ['flippermove_setMovementPair', 'motor_pair = MotorPair()'], ['flippermove_setDistance', 'motor_pair.set_motor_rotation()'], ['flippersound_playSoundUntilDone', 'app.start_sound()'], ['flippersound_playSound', 'app.play_sound()'], ['flippersound_beepForTime', 'hub.speaker.beep()'], ['flippersound_beep', 'hub.speaker.start_beep()'], ['flippersound_stopSound', 'hub.speaker.stop()'], ['control_wait', 'wait_for_seconds()'], ['control_wait_until', 'wait_until()'], ['flippersensors_resetYaw', 'hub.motion_sensor.reset_yaw_angle()'], ['flippersensors_resetTimer', 'timer.reset()']]
+functionslist=[['flippermotor_motorGoDirectionToPosition', 'motor.run_to_position()'], ['flippermotor_motorStartDirection', 'motor.start()'], ['flippermotor_motorSetSpeed', 'motor.set_default_speed()'], ['flippermove_move', 'motor_pair.move()'], ['flippermove_steer', 'motor_pair.move_tank()'], ['flippermove_startSteer', 'motor_pair.start_tank()'], ['flippermove_stopMove', 'motor_pair.stop()'], ['flippermove_movementSpeed', 'motor_pair.set_default_speed()'], ['flippermove_setMovementPair', 'motor_pair = MotorPair()'], ['flippermove_setDistance', 'motor_pair.set_motor_rotation()'], ['flippersound_playSoundUntilDone', 'app.start_sound()'], ['flippersound_playSound', 'app.play_sound()'], ['flippersound_beepForTime', 'hub.speaker.beep()'], ['flippersound_beep', 'hub.speaker.start_beep()'], ['flippersound_stopSound', 'hub.speaker.stop()'], ['control_wait', 'wait_for_seconds()'], ['control_wait_until', 'wait_until()'], ['flippersensors_resetYaw', 'hub.motion_sensor.reset_yaw_angle()'], ['flippersensors_resetTimer', 'timer.reset()'],['flipperdisplay_ledText', 'hub.light_matrix.write()'],['flipperdisplay_displayOff', 'hub.light_matrix.off()'],['flipperdisplay_ledOn', 'hub.light_matrix.set_pixel()'], ['flipperdisplay_centerButtonLight', 'hub.status_light.on()'], ['flipperdisplay_ultrasonicLightUp', 'distance_sensor.light_up()']]
 
 #Define a função de conversão
 def convert(jsonobj):
@@ -22,6 +23,9 @@ hub = PrimeHub()
 app = App()
 motor = Motor('A')
 motor_pair = MotorPair('B', 'A')
+distance_sensor = DistanceSensor('C')
+color = ColorSensor('D')
+force = ForceSensor('E')
 
 '''
     for v in data['targets'][1]['variables']:
@@ -59,6 +63,25 @@ motor_pair = MotorPair('B', 'A')
             item=str(data['targets'][1]['blocks'][i]['inputs']['ITEM'][1][1])
             index=str(int(data['targets'][1]['blocks'][i]['inputs']['INDEX'][1][1])-1)
             program=f"{program}{listo}.insert({index},'{item}')\n"
+        #----LIGHT MATRIX PARSER/ANALIZADOR DA MATRIZ DE LUZ----#
+        if data['targets'][1]['blocks'][i]['opcode'] == "flipperdisplay_ledImageFor":
+            matrixlight=numpy.mat(numpy.array(list(data['targets'][1]['blocks'][data['targets'][1]['blocks'][i]["inputs"]['MATRIX'][1]]['fields']["field_flipperdisplay_custom-matrix"][0])))
+            matrixlight=matrixlight.reshape(5,5)
+            newmatrixlight=[]
+            for ind in range(0,5):
+                listmatrix=list(map(int, matrixlight.tolist()[ind]))
+                listmatrix = [itemint * 10 for itemint in listmatrix]
+                newmatrixlight.append(listmatrix)
+            program=program+f"for indexrow, row in enumerate({str(newmatrixlight)}):\n   for indexcol, column in enumerate(row):\n      hub.light_matrix.set_pixel(indexrow, indexcol, brightness=column)\nwait_for_seconds({int(data['targets'][1]['blocks'][i]['inputs']['VALUE'][1][1])})\nhub.light_matrix.off()\n"
+        if data['targets'][1]['blocks'][i]['opcode'] == "flipperdisplay_ledImage":
+            matrixlight=numpy.mat(numpy.array(list(data['targets'][1]['blocks'][data['targets'][1]['blocks'][i]["inputs"]['MATRIX'][1]]['fields']["field_flipperdisplay_custom-matrix"][0])))
+            matrixlight=matrixlight.reshape(5,5)
+            newmatrixlight=[]
+            for ind in range(0,5):
+                listmatrix=list(map(int, matrixlight.tolist()[ind]))
+                listmatrix = [itemint * 10 for itemint in listmatrix]
+                newmatrixlight.append(listmatrix)
+            program=program+f"for indexrow, row in enumerate({str(newmatrixlight)}):\n   for indexcol, column in enumerate(row):\n      hub.light_matrix.set_pixel(indexrow, indexcol, brightness=column)\n"
         #Verifica a lista de funções.
         for function in functionslist:
             #Define a lista de inputs.
